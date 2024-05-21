e6c78572cec2e0568f50ec4cf32db0b34ee73b56
tjdan805 <tjdan805@gmail.com>
2024-05-21 15:16:10 +0900

    mock

--- src/test/java/com/example/demo/service/BoardServiceTest.java
+++ src/test/java/com/example/demo/service/BoardServiceTest.java
@@ -3,39 +3,52 @@ package com.example.demo.service;
 import com.example.demo.domain.entity.Board;
 import com.example.demo.repository.BoardRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class BoardServiceTest {
-    @Autowired
-    private BoardService boardService;
-    @Autowired
+    @InjectMocks
+    private BoardServiceImpl boardService;
+    @Mock
     private BoardRepository boardRepository;
     @Test
     void deleteById() {
 //        case 2: 있을 때
         // given
-        Board save = boardRepository.save(new Board(null, "test", "test"));
-        Long id = save.getId();
+        Long id = 1L;
+        BDDMockito.doNothing().when(boardRepository).deleteById(id);
+        BDDMockito.given(boardRepository.findById(id))
+                .willReturn(Optional.of(new Board(1L,null,null)));
 
         //when
         boardService.deleteById(id);
 
         //then
-        assertEquals(0, boardRepository.findAll().size());
+
     }
     @Test
     void deleteByIdFail() {
 //        case 1: id 이런 것이 없을때 IllegalArgumentException
         // given
-        Long id = 80000L;
+        Long id = 5000L;
+        BDDMockito.given(boardRepository.findById(id))
+                .willReturn(Optional.empty());
 
         //when & then
         assertThrows(IllegalArgumentException.class, () -> {
             boardService.deleteById(id);
         });
+
     }
 }
