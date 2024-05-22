5c9e01d1869b50c1b58a1306d757d187bed2e3f7
tjdan805 <tjdan805@gmail.com>
2024-05-22 15:42:49 +0900

    test

--- src/test/java/com/example/demo/controller/BoardControllerTest.java
+++ src/test/java/com/example/demo/controller/BoardControllerTest.java
@@ -1,30 +1,64 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.response.BoardResponse;
+import com.example.demo.service.BoardServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+
+
+@WebMvcTest(BoardController.class)
 class BoardControllerTest {
+    @MockBean
+    private BoardServiceImpl boardService;
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Test
-    void getAllBoard() {
-
+    void getAllBoard() throws Exception {
+//        given
+//        boardService.getAll()  list [3]
+        BDDMockito.given(boardService.getAll())
+                .willReturn(List.of(
+                        new BoardResponse(1l, "test1", "test1"),
+                        new BoardResponse(2l, "test2", "test2"),
+                        new BoardResponse(3l, "test3", "test3")
+                ));
+//        when & then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/boards"))// axios.get("/api/boards")
+                .andExpect(
+                        MockMvcResultMatchers.jsonPath("$[0].id").value(1)
+                ) // 검증
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(3))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print()); // 하는거
+//
+//        body : [1,2,3]
+//
     }
 
     @Test
     void getByIdBoard() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/boards/1"))
-                .andExpect(MockMvcResultMatchers.status().is5xxServerError());
+
     }
 
     @Test
--- src/test/java/com/example/demo/service/BoardServiceImplTest.java
+++ src/test/java/com/example/demo/service/BoardServiceImplTest.java
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
 //
 import java.util.List;
 import java.util.Optional;
